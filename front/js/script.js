a16f3c91b48b58e4f1d19f9a5d5f62eff1145dc5
EP3mike <michaelamezcua99@gmail.com>
2022-06-23 00:51:16 -0700

    added to product GET script

--- front/js/script.js
+++ front/js/script.js
@@ -1,30 +1,79 @@
+const apiUrl = 'http://localhost:3000/api/products'
+
 //GET request function using fetch, will request all products contained by api
 async function fetchProducts() {
     try {
-        const response = await fetch('http://localhost:3000/api/products');
+        const response = await fetch(apiUrl);
 
         if(!response.ok) {
             throw new Error('Failed to fetch Kanap products: ${response.status}');
         }
-        return response.json();
+
+        // const products = await response.json();
+        // return products;
+        return await response.json();
     }   catch(e) {
         console.log(e);
     }
 
 }
 
-//function to insert products from api into dom, takes html element id parameter
+//function to insert products from api into dom, takes html element id parameter to insert into
 
-function insertProducts(containerElementId) {
-    const containerElementId = document.getElementById(containerElementId);
+function insertProducts(productContainerElementId) {
+    const productContainerElementId = document.getElementById(productContainerElementId);
 
-    if(!containerElementId) {
+    if(!productContainerElementId) {
         return 0;
     }
 
-    fetchProducts().then(posts => {
+    fetchProducts()
+    .then(products => {
+        if(!products) {)
+            productContainerElementId.innerHTML = 'No products fetched!';
+            return 0;
+        }
 
-    }).catch(e => {
-        console.log(e);
+        for( const product of products) {
+            productContainerElementId.appendChild(createProductElement(product));
+        }
+
+
     })
+    .catch(e => {
+        console.log(e);
+    });
 }
+
+//function to create a new product dom element for the returned info from api, ie new img,title,description
+function createProductElement(product) {
+    const anchorElement = document.createElement('a');
+    anchorElement.setAttribute('href', `${apiUrl}/${product.id}`);
+    anchorElement.setAttribute('target', '_blank');
+
+
+    const articleElement = document.createElement('article');
+    anchorElement.appendChild(articleElement);
+
+    const imageElement = document.createElement('img');
+    imageElement.setAttribute('src', product.imageURL);
+    imageElement.setAttribute('alt', product.altTxt);
+    articleElement.appendChild(imageElement);
+
+
+    const titleElement = document.createElement('h3');
+    titleElement.innerHTML = product.name;
+    articleElement.appendChild(titleElement);
+
+    const descriptionElement = document.createElement('p');
+    descriptionElement.innerHTML = product.description;
+    articleElement.appendChild(descriptionElement);
+
+
+    return articleElement;
+
+    
+}
+
+
+insertProducts('items');
